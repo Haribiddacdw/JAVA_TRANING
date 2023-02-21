c9c08cb9bc53e0dde94aaa94ec6c7061cd2b6e18
Haribiddacdw <hariprasath.viswanathan@cdw.com>
2023-02-21 23:06:11 +0530

    Exercise 2
    Exercise 2 added till question 6

---
+++ Exercise_2/Q2/Sample.java
@@ -0,0 +1,23 @@
+package Q2;
+
+public class Sample {
+    public void mth1(){
+        mth2();
+        System.out.println("caller");
+    }
+    public void mth2(){
+        try{
+            return;
+        }catch(Exception e){
+            System.out.println("catch-mth2");
+        }
+        finally{
+           System.out.println("finally-mth2");
+        }
+    }
+    public static void  main(String p[])
+    {
+        Sample s = new Sample();
+        s.mth1();
+    }
+}
